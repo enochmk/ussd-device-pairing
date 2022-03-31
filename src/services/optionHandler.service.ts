d650f30c0ee85072952b705200b508b2311bad8d
Enoch Klufio <enochmk94@gmail.com>
2022-03-31 15:01:19 +0000

    feat: :sparkles: handler to handle option request

--- src/services/optionHandler.service.ts
+++ src/services/optionHandler.service.ts
@@ -1,6 +1,9 @@
 import { getLastSession } from './../models/redisDB';
 import messages from '../utils/messages/app.messages';
 import option1 from './options/option1.service';
+import option2 from './options/option2.service';
+import option3 from './options/option3.service';
+import option4 from './options/option4.service';
 
 const optionHandler = async (sessionID: string) => {
 	const request = await getLastSession(sessionID);
@@ -8,6 +11,12 @@ const optionHandler = async (sessionID: string) => {
 	switch (request.option) {
 		case '1':
 			return await option1(request);
+		case '2':
+			return await option2(request);
+		case '3':
+			return await option3(request);
+		case '4':
+			return await option4(request);
 		default:
 			return { menu: messages.INVALID_OPTION, flag: 2 };
 	}
